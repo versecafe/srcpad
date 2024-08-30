a888346bcde091feeb92b3a8da2782a5c4eb2679
versecafe <147033096+versecafe@users.noreply.github.com>
2024-08-30 01:50:24 -0500

    tiny patch for windows

--- packages/api/src/constants.ts
+++ packages/api/src/constants.ts
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 const _filename = fileURLToPath(import.meta.url);
 
 export const DIST_DIR = path.dirname(path.dirname(_filename));
-export const UI_DIR = path.join(path.dirname(DIST_DIR), "ui/dist");
+export const UI_DIR = path.join(path.dirname(DIST_DIR), "ui", "dist");
 export const DRIZZLE_DIR = path.join(DIST_DIR, "drizzle");
 export const SRCPAD_DIR = path.join(os.homedir(), ".srcpad");
 export const SRCPADS_DIR = path.join(SRCPAD_DIR, "srcpads");
