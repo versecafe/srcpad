4409af0863593d80c05840895685938ae24a9818
versecafe <147033096+versecafe@users.noreply.github.com>
2024-09-02 15:54:34 -0500

    cleanup the logic here

--- packages/ui/.eslintrc.cjs
+++ packages/ui/.eslintrc.cjs
@@ -1,7 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@repo/configs/eslint/react-internal.js"],
+  extends: [require.resolve("@repo/configs/eslint/react-internal.js")],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "./tsconfig.lint.json",
