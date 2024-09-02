5ce1ed65d00c28ea414bfaf30e2f4b1f55c70a7f
versecafe <147033096+versecafe@users.noreply.github.com>
2024-09-02 18:58:58 -0500

    fix the worlds weirdest resolution issue

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -2,7 +2,7 @@
 import { render } from "solid-js/web";
 import "solid-devtools";
 
-import { App } from "@/app.tsx";
+import { App } from "@/App.tsx";
 
 const root = document.getElementById("root");
 
